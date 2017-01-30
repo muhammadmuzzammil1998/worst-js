61a014cc73972654613c02b01be654d7af09d1de
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2017-01-30 06:32:43 +0530

    Update worst.js

--- src/worst.js
+++ src/worst.js
@@ -1,1 +1,12 @@
+/*
+MIT License
+
+Copyright (c) 2016 Muhammad Muzzammil (http://98.hol.es/)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this script and associated documentation files (the "Script"), to deal in the script without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the script, and to permit persons to whom the script is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SCRIPT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SCRIPT OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 var _0x53bb=["\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x62\x6F\x64\x79",""];document[_0x53bb[1]][_0x53bb[0]]= _0x53bb[2]
